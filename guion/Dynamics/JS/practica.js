6ed9645f61c2aa5b42c1b627a0bfe8bd429185a7
AlbertoFalcon <falcon_ga@ciencias.unam.mx>
2025-06-12 07:54:15 -0600

    Ejercicios del Guion de Kung-Fu Panda

---
+++ guion/Dynamics/JS/practica.js
@@ -0,0 +1,13 @@
+// Aquí va el código para poder ver la transcripción de la película Kung-Fu Panda !!!!!!
+/**
+ * Plan de acción:
+ * 1.- Seleccionar el <div> en el que están contenidos todos los elementos <p>
+ * 
+ * 2.- Seleccionar todos los elementos <p> que tengan la clase *****.  class = "*****"
+ * 
+ * 3.- Lo anterior nos dará un _____________ que podremos iterar como arreglo
+ * 
+ * 4.- Por cada entrada de la colección, debemos crear un elemento <p>, le añadimos el respectivo
+ * fragmento del guion, añadimos la clase ************, para darle diseño. Finalmente, debemos
+ * agregarlo al DOM.
+ */
